0066061b985ffa45638d4a47233fc80605b2ba9f
Yaroslav Vasylenko <gloomy.699@gmail.com>
2020-07-22 20:18:06 +0300

    добавляет prettierrc

--- js/hw_1-part-2.js
+++ js/hw_1-part-2.js
@@ -1,11 +1,1 @@
-(() => {
-  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
-    e.preventDefault();
 
-    new FormData(e.currentTarget).forEach((value, name) =>
-      console.log(`${name}: ${value}`),
-    );
-
-    e.currentTarget.reset();
-  });
-})();

07e8a055d294477d0de08911318431434673c931
Yaroslav Vasylenko <gloomy.699@gmail.com>
2020-07-31 01:30:59 +0300

    3 задача 2 ДЗ

--- js/hw_1-part-2.js
+++ js/hw_1-part-2.js
@@ -1,1 +1,51 @@
+//--------------------------------------------------------------------------------------//
 
+// function findLongestWord(string = '') {
+//   // Write code under this line
+//   const wordSplit = string.split(' ');
+
+//   let longestWord = '';
+
+//   for (let i = 0; i < wordSplit.length; i += 1)
+//     if (wordSplit.length > longestWord) {
+//       longestWord = wordSplit.length;
+//     }
+
+//   return longestWord;
+// }
+//--------------------------------------------------------------------------------------//
+
+// function findLongestWord(string = '') {
+//   let longestWord = '';
+//   string.split(' ');
+
+//   for (longestWord.length of string.split('')) {
+//     if (string > longestWord.length) {
+//       longestWord.length = string;
+//     }
+//   }
+
+//   return longestWord;
+// }
+//--------------------------------------------------------------------------------------//
+
+function findLongestWord(string = '') {
+  string.split(' ');
+  let LongestWord = '';
+  for (const words of string.split(' ')) {
+    if (words > LongestWord) {
+      LongestWord = words;
+    }
+  }
+
+  return LongestWord;
+}
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+//'jumped'
+
+console.log(findLongestWord('Google do a roll'));
+//'Google'
+
+console.log(findLongestWord('May the force be with you'));
+//'force'

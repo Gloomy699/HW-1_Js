9505f027c6a1909cb0e74645b649fa0224056a9e
Yaroslav Vasylenko <gloomy.699@gmail.com>
2020-07-31 23:57:19 +0300

    решает задачи с 3 по 5

--- js/hw_1-part-2.js
+++ js/hw_1-part-2.js
@@ -1,17 +1,17 @@
 //--------------------------------------------------------------------------------------//
 
 // function findLongestWord(string = '') {
-//   // Write code under this line
-//   const wordSplit = string.split(' ');
+//     // Write code under this line
+//     const wordSplit = string.split(' ');
 
-//   let longestWord = '';
+//     let longestWord = '';
 
-//   for (let i = 0; i < wordSplit.length; i += 1)
-//     if (wordSplit.length > longestWord) {
-//       longestWord = wordSplit.length;
-//     }
+//     for (let i = 0; i < string.length; i += 1)
+//         if (wordSplit.length > longestWord {
+//             longestWord = wordSplit.length;
+//         }
 
-//   return longestWord;
+//     return longestWord;
 // }
 //--------------------------------------------------------------------------------------//
 
@@ -29,23 +29,86 @@
 // }
 //--------------------------------------------------------------------------------------//
 
-function findLongestWord(string = '') {
-  string.split(' ');
-  let LongestWord = '';
-  for (const words of string.split(' ')) {
-    if (words > LongestWord) {
-      LongestWord = words;
-    }
-  }
+// function findLongestWord(string = '') {
+//   string.split(' ');
+//   let LongestWord = '';
+//   for (const words of string.split(' ')) {
+//     if (words > LongestWord) {
+//       LongestWord = words;
+//     }
+//   }
 
-  return LongestWord;
-}
+//   return LongestWord;
+// }
 
-console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
-//'jumped'
+// function findLongestWord(string = ' ') {
+//   const wordSplit = string.split(' ');
+//   let longestWord = '';
 
-console.log(findLongestWord('Google do a roll'));
-//'Google'
+//   console.log(wordSplit);
 
-console.log(findLongestWord('May the force be with you'));
-//'force'
+//   for (let i = 0; i < wordSplit.length; i += 1)
+//     if (wordSplit[i].length > longestWord.length) {
+//       longestWord = wordSplit[i];
+//     }
+
+//   return longestWord;
+// }
+
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// //'jumped'
+
+// console.log(findLongestWord('Google do a roll'));
+// //'Google'
+
+// console.log(findLongestWord('May the force be with you'));
+// //'force'
+
+//------------------------------------------------------------------------------------////------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------////------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//задача 4//--------------------------------------------------------------------------------//
+
+// function formatString(string, maxLength = 40) {
+//   // Write code under this line
+//   const points = ['...'];
+
+//   for (let i = 0; i < string.length; i += 1);
+//   if (string.length > maxLength) {
+//     string = string.slice(0, maxLength).concat(points);
+//   }
+
+//   return string;
+// }
+
+// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// // 'Curabitur ligula sapien, tincidunt non.'
+
+// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// // 'Vestibulum facilisis, purus nec pulvinar...'
+
+// console.log(
+//   formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30),
+// );
+// // 'Vestibulum facilisis, purus ne...'
+
+//------------------------------------------------------------------------------------////------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------////------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//задача 5//--------------------------------------------------------------------------------//
+// function checkForSpam(str) {
+//     // Write code under this line
+
+//     return str.toLowerCase().includes('spam') || str.toLowerCase().includes('sale') ? true : false;
+
+// }
+
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+// console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+//------------------------------------------------------------------------------------////------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------////------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//задача 5//--------------------------------------------------------------------------------//
